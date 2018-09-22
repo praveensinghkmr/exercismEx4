8e9e825550a119068d9de4810f23c66d5381f305
Praveenkumar Singh <praveenkumar.singh@in.abb.com>
2018-09-22 17:30:05 +0530

    Implemented Logic

--- SumOfMultiples.cs
+++ SumOfMultiples.cs
@@ -5,6 +5,18 @@ public static class SumOfMultiples
 {
     public static int Sum(IEnumerable<int> multiples, int max)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        int sum = 0;
+        for(int i = 1; i<max; i++)
+        {
+            foreach (int x in multiples)
+            {
+                if (i%x == 0)
+                {
+                    sum = sum + i;
+                    break;
+                }
+            }
+        }
+        return sum;
     }
 }
