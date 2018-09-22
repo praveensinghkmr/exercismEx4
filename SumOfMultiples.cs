4ca2b4fd56ac50f8c656245b62a0af01d39e5cee
Praveenkumar Singh <praveenkumar.singh@in.abb.com>
2018-09-22 19:01:21 +0530

    Using Linq

--- SumOfMultiples.cs
+++ SumOfMultiples.cs
@@ -1,22 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public static class SumOfMultiples
 {
     public static int Sum(IEnumerable<int> multiples, int max)
     {
-        int sum = 0;
-        for(int i = 1; i<max; i++)
-        {
-            foreach (int x in multiples)
-            {
-                if (i%x == 0)
-                {
-                    sum = sum + i;
-                    break;
-                }
-            }
-        }
-        return sum;
+        return Enumerable.Range(0, max).Where(f => multiples.Any(h => f % h == 0)).Sum();
     }
 }
